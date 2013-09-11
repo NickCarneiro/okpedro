b1ea42cab725c8979cc4004a7d3cf88303b3bac7
Nick Carneiro <burthawk101@gmail.com>
2013-09-11 11:07:12 -0500

    added charged boolean

--- okpapp/models.py
+++ okpapp/models.py
@@ -8,6 +8,7 @@ class Application(models.Model):
     requests = models.TextField()
     apply_datetime = models.DateTimeField(auto_now_add=True)
     stripe_token = models.TextField(max_length=400)
+    charged = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.email_address
